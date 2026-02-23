5882c3e8bc578dc6cb33c93970bcbab4d50ca953
Rabera <raberakaunda@gmail.com>
2026-02-23 04:51:43 +0300

    mapped dates pending styling

--- src/pages/MyEnergy/tabs/PurchaseHistory.jsx
+++ src/pages/MyEnergy/tabs/PurchaseHistory.jsx
@@ -3,6 +3,13 @@ import styles from "./PurchaseHistory.module.css";
 
 const FILTERS = ["Last 7 days", "Last 30 days", "Last 90 days"];
 
+const PURCHASE = [
+  { id: 1, date: "Today, 2:34 PM", ammount: "₦5,500", units: "+50kWh" },
+  { id: 2, date: "Feb 6, 2026", ammount: "₦5,500", units: "+50kWh" },
+  { id: 3, date: "Jan 30, 2026", ammount: "₦5,500", units: "+50kWh" },
+  { id: 4, date: "Jan 30, 2026", ammount: "₦5,500", units: "+50kWh" },
+];
+
 function PurchaseHistory() {
   const [activeFilter, setActiveFilter] = useState("Last 30 days");
   return (
@@ -18,7 +25,20 @@ function PurchaseHistory() {
           </button>
         ))}
       </div>
-      <p>Purchase History</p>
+      <div className={styles.section}>
+        <h3 className={styles.sectionTitle}>Purchase History</h3>
+
+        {PURCHASE.map(({ id, date, amount, units }) => (
+          <div key={id} className={styles.purchaseItem}>
+            <span>icon</span>
+            <div>
+              <p>{date}</p>
+              <p>{amount}</p>
+            </div>
+            <span>{units}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }

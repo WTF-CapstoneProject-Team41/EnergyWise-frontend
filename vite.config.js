fad0e510d17ed481520300bf30d2553ba94d0bed
Rabera <raberakaunda@gmail.com>
2026-02-28 03:04:05 +0300

    trial 3

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/<WTF-CapstoneProject-Team41/EnergyWise-frontend>/",
+
   // optimizeDeps: { include: ['recharts'] }
 });
